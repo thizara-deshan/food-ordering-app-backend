332c35f990731b9aa90dcea3bd51b96a8fe39b76
Thizara <thizaradeshan@gmail.com>
2024-08-10 15:49:31 +0530

    orderRoute feature

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,42 +1,42 @@
-import Stripe from "stripe";
-import { Request, Response } from "express";
-import Restaurant from "../models/restaurant";
+// import Stripe from "stripe";
+// import { Request, Response } from "express";
+// import Restaurant from "../models/restaurant";
 
-const STRIPE = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-const FRONTEND_URL = process.env.FRONTEND_URL as string;
+// const STRIPE = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+// const FRONTEND_URL = process.env.FRONTEND_URL as string;
 
-type CheckoutSessionRequest = {
-  cartItems: {
-    menuitemId: string;
-    name: string;
-    quantity: number;
-  }[];
-  deliveryDetails: {
-    email: string;
-    name: string;
-    addressLine1: string;
-    city: string;
-  };
-  restaurantId: string;
-};
+// type CheckoutSessionRequest = {
+//   cartItems: {
+//     menuitemId: string;
+//     name: string;
+//     quantity: number;
+//   }[];
+//   deliveryDetails: {
+//     email: string;
+//     name: string;
+//     addressLine1: string;
+//     city: string;
+//   };
+//   restaurantId: string;
+// };
 
-const createCheckoutSession = async (req: Request, res: Response) => {
-  try {
-    const checkoutSessionRequest = req.body as CheckoutSessionRequest;
-    const restaurant = await Restaurant.findById(
-      checkoutSessionRequest.restaurantId
-    );
+// const createCheckoutSession = async (req: Request, res: Response) => {
+//   try {
+//     const checkoutSessionRequest = req.body as CheckoutSessionRequest;
+//     const restaurant = await Restaurant.findById(
+//       checkoutSessionRequest.restaurantId
+//     );
 
-    if (!restaurant) {
-      return res.status(404).json({ message: "Restaurant Not Found" });
-    }
+//     if (!restaurant) {
+//       return res.status(404).json({ message: "Restaurant Not Found" });
+//     }
 
-    const lineItems = createLineItems(
-      checkoutSessionRequest,
-      restaurant.menuitems
-    );
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json({ message: error.raw.message });
-  }
-};
+//     const lineItems = createLineItems(
+//       checkoutSessionRequest,
+//       restaurant.menuitems
+//     );
+//   } catch (error: any) {
+//     console.log(error);
+//     res.status(500).json({ message: error.raw.message });
+//   }
+// };
--- src/routes/OrderRoute.ts
+++ src/routes/OrderRoute.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import { jwtCheck, jwtparse } from "../middleware/auth";
+// import express from "express";
+// import { jwtCheck, jwtparse } from "../middleware/auth";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post(
-  "/checkout/create-checkout-session",
-  jwtCheck,
-  jwtparse,
-  OrderController.createCheckoutSession
-);
+// router.post(
+//   "/checkout/create-checkout-session",
+//   jwtCheck,
+//   jwtparse,
+//   OrderController.createCheckoutSession
+// );
